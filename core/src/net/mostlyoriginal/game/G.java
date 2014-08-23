b7249b985dad049eb526fd8d6ba2958cc585f651
Daan <daan@mostlyoriginal.net>
2014-08-23 19:31:16 +0200

    Change to youtube 720p resolution.

--- core/src/net/mostlyoriginal/game/G.java
+++ core/src/net/mostlyoriginal/game/G.java
@@ -5,8 +5,8 @@ package net.mostlyoriginal.game;
  */
 public class G {
 
-    public static final int CANVAS_WIDTH = 800;
-    public static final int CANVAS_HEIGHT = 480;
-    public static final int SCREEN_TOP = CANVAS_HEIGHT / MainScreen.CAMERA_ZOOM_FACTOR;
-    public static final int SCREEN_RIGHT = CANVAS_WIDTH / MainScreen.CAMERA_ZOOM_FACTOR;
+    public static final int CANVAS_WIDTH = 1280;
+    public static final int CANVAS_HEIGHT = 720;
+    public static final int SCREEN_HEIGHT = CANVAS_HEIGHT / MainScreen.CAMERA_ZOOM_FACTOR;
+    public static final int SCREEN_WIDTH = CANVAS_WIDTH / MainScreen.CAMERA_ZOOM_FACTOR;
 }
