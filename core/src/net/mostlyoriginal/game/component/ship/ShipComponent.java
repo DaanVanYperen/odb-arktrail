4d8173a88234aed3c82f49396884dd71b17a8d53
Daan <daan@mostlyoriginal.net>
2014-08-24 11:29:32 +0200

    Start with a basic ship.

--- core/src/net/mostlyoriginal/game/component/ship/ShipComponent.java
+++ core/src/net/mostlyoriginal/game/component/ship/ShipComponent.java
@@ -12,10 +12,11 @@ public class ShipComponent extends Component {
     public int gridY;
     public int gridX;
 
-    public ShipComponent(Type type, int gridX, int gridY) {
+    public ShipComponent(Type type, int gridX, int gridY, State state) {
         this.type = type;
         this.gridX = gridX;
         this.gridY = gridY;
+        this.state = state;
     }
 
     public enum State {
@@ -24,19 +25,32 @@ public class ShipComponent extends Component {
     }
 
     public enum Type {
-        EXPANSION_SLOT(null, false),
-        BUNKS("c-bunks", true),
-        MEDBAY("c-medbay", true),
-        HYDROPONICS("c-hydroponics", true),
-        STORAGEPOD("c-storagepod", true),
-        ENGINE("c-engine", true),
-        RAMSCOOP("c-ramscoop", true);
+        EXPANSION_SLOT(null, false, 500, 0),
+        BUNKS("c-bunks", true, 500, 0),
+        MEDBAY("c-medbay", true, 500, 0),
+        HYDROPONICS("c-hydroponics", true, 500, 0),
+        STORAGEPOD("c-storagepod", true, 500, 0),
+        ENGINE("c-engine", true, 550, -8),
+        RAMSCOOP("c-ramscoop", true, 550, 0);
 
         public final String animId;
         public final boolean buildable;
+        public final String placedAnimId;
+        public final int layer;
+        public final int xOffset;
 
-        Type(String animId, boolean buildable) {
+        /**
+         *
+         * @param animId
+         * @param buildable
+         * @param layer
+         * @param xOffset offset when placed on the ship (for engines).
+         */
+        Type(String animId, boolean buildable, int layer, int xOffset) {
             this.animId = animId;
+            this.layer = layer;
+            this.xOffset = xOffset;
+            this.placedAnimId = animId + "-placed";
             this.buildable = buildable;
         }
     }
--- core/src/net/mostlyoriginal/game/manager/ShipComponentSystem.java
+++ core/src/net/mostlyoriginal/game/manager/ShipComponentSystem.java
@@ -5,8 +5,12 @@ import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.annotations.Wire;
 import com.artemis.systems.EntityProcessingSystem;
+import com.artemis.utils.EntityBuilder;
+import net.mostlyoriginal.api.component.basic.Bounds;
 import net.mostlyoriginal.api.component.basic.Pos;
+import net.mostlyoriginal.api.component.graphics.Anim;
 import net.mostlyoriginal.game.component.ship.ShipComponent;
+import net.mostlyoriginal.game.component.ui.Clickable;
 
 /**
  * Holds a spatial map of the ship.
@@ -19,9 +23,10 @@ public class ShipComponentSystem extends EntityProcessingSystem {
     public static final int MARGIN_TOP = 32;
     public static final int MARGIN_LEFT = 32;
     protected ComponentMapper<Pos> mPos;
+    protected ComponentMapper<Anim> mAnim;
 
     public ShipComponentSystem() {
-        super(Aspect.getAspectForAll(ShipComponent.class, Pos.class));
+        super(Aspect.getAspectForAll(ShipComponent.class, Pos.class, Anim.class));
     }
 
     protected ComponentMapper<ShipComponent> mc;
@@ -34,38 +39,52 @@ public class ShipComponentSystem extends EntityProcessingSystem {
     @Override
     protected void initialize() {
         super.initialize();
+
+        // initialize basic ship.
+        // create test expansion slot.
+        createComponent(5, 5, ShipComponent.Type.ENGINE, ShipComponent.State.CONSTRUCTED);
+        createComponent(6, 5, ShipComponent.Type.STORAGEPOD, ShipComponent.State.CONSTRUCTED);
+        createComponent(6, 4, ShipComponent.Type.BUNKS, ShipComponent.State.CONSTRUCTED);
+        createComponent(6, 6, ShipComponent.Type.BUNKS, ShipComponent.State.CONSTRUCTED);
+    }
+
+    /**
+     * attempts to create a component at coordinates. will fail if out of bounds or already one there.
+     */
+    private Entity createComponent(int gridY, int gridX, ShipComponent.Type expansionSlot, ShipComponent.State state) {
+        if (gridY < 0 || gridX < 0 || gridY >= MAX_X || gridX >= MAX_Y) return null;
+        if (get(gridY, gridX) == null) {
+            return new EntityBuilder(world).with(new Pos(), new Anim(), new ShipComponent(expansionSlot, gridY, gridX, state), new Bounds(0, 0, 8, 8), new Clickable()).build();
+        }
+        return null;
     }
 
     @Override
     protected void inserted(Entity e) {
-        if ( mc.has(e))
-        {
+        if (mc.has(e)) {
             ShipComponent shipComponent = mc.get(e);
-            if ( emap[shipComponent.gridY][shipComponent.gridX] == null ) {
+            if (emap[shipComponent.gridY][shipComponent.gridX] == null) {
                 set(shipComponent.gridX, shipComponent.gridY, e);
             }
         }
         super.inserted(e);
     }
 
-    public Entity get(int x, int y)
-    {
-        if ( x < 0 || y < 0 || x >= MAX_X || y >= MAX_Y ) return null;
+    public Entity get(int x, int y) {
+        if (x < 0 || y < 0 || x >= MAX_X || y >= MAX_Y) return null;
         return emap[y][x];
     }
 
     public void set(int x, int y, Entity e) {
-        if ( x < 0 || y < 0 || x >= MAX_X || y >= MAX_Y ) return;
+        if (x < 0 || y < 0 || x >= MAX_X || y >= MAX_Y) return;
         emap[y][x] = e;
     }
 
     @Override
     protected void removed(Entity e) {
-        if ( mc.has(e))
-        {
+        if (mc.has(e)) {
             ShipComponent shipComponent = mc.get(e);
-            if ( emap[shipComponent.gridY][shipComponent.gridX] == e )
-            {
+            if (emap[shipComponent.gridY][shipComponent.gridX] == e) {
                 set(shipComponent.gridX, shipComponent.gridY, null);
             }
         }
@@ -76,7 +95,13 @@ public class ShipComponentSystem extends EntityProcessingSystem {
     protected void process(Entity e) {
         Pos pos = mPos.get(e);
         ShipComponent shipComponent = mc.get(e);
-        pos.x = shipComponent.gridX * 8 + MARGIN_TOP;
-        pos.y = shipComponent.gridY * 8 + MARGIN_LEFT;
+        pos.x = shipComponent.gridX * 8 + MARGIN_LEFT + shipComponent.type.xOffset;
+        pos.y = shipComponent.gridY * 8 + MARGIN_TOP;
+
+        Anim anim = mAnim.get(e);
+        anim.layer = shipComponent.type.layer;
+        if (shipComponent.type.animId != null) {
+            anim.id = shipComponent.type.placedAnimId;
+        }
     }
 }
