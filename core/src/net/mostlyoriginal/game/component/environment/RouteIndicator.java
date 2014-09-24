4db1106ee90c8300252434fa2f8be99b4669703d
DaanVanYperen <daan@mostlyoriginal.net>
2014-09-24 21:34:21 +0200

    Upgrade to 0.7.1

--- core/src/net/mostlyoriginal/game/component/environment/RouteIndicator.java
+++ core/src/net/mostlyoriginal/game/component/environment/RouteIndicator.java
@@ -1,10 +1,12 @@
 package net.mostlyoriginal.game.component.environment;
 
 import com.artemis.Component;
+import com.artemis.annotations.PackedWeaver;
 
 /**
  * @author Daan van Yperen
  */
+@PackedWeaver
 public class RouteIndicator extends Component {
     public int at;
 }
